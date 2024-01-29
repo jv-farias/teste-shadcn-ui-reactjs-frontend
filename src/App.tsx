c756a6ddb2e5490b21dc36b6001cb36a8d9e6d88
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-29 14:14:14 +0000

    :sparkles: feat: interface done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,84 @@
-import './App.css'
 
-function App() {
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Search, PlusCircle } from 'lucide-react';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table";
+import { Dialog, DialogFooter, DialogHeader, DialogContent, DialogTitle, DialogTrigger, DialogClose } from "./components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { Label } from "./components/ui/label";
 
-
+export function App() {
   return (
-    <>
-    
-    </>
+    <div>
+      <div className="p-6 max-w-4xl mx-auto space-y-4">
+        <h1 className="text-4xl font-bold">Produtos</h1>
+        <div className="flex items-center justify-between">
+          <form className="flex items-center gap-2">
+            <Input name="id" placeholder="ID do Produto" />
+            <Input name="name" placeholder="Nome do Produto" />
+            <Button type="submit" variant="link">
+              <Search className="w-4 h-4 mr-2" />
+              Filtrar Resultados</Button>
+          </form>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>
+                <PlusCircle className="w-4 h-4 mr-2" />
+                Novo Produto
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Novo Produto</DialogTitle>
+                <DialogDescription>Adicionar um novo produto</DialogDescription>
+              </DialogHeader>
+              <form className="space-y-6">
+                <div className="grid grid-cols-4 items-center text-right gap-3">
+                  <Label htmlFor="name" >Produto</Label>
+                  <Input className="col-span-3" id="name" />
+                </div>
+
+                <div className="grid grid-cols-4 items-center text-right gap-3">
+                  <Label htmlFor="price" >Preço</Label>
+                  <Input className="col-span-3" id="price" />
+                </div>
+
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="destructive">Cancelar</Button>
+                  </DialogClose>
+                  <Button type="submit">Salvar</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
+
+
+        </div>
+        <div className="border rounded-lg p-2" >
+          <Table>
+            <TableHeader>
+              <TableHead>ID</TableHead>
+              <TableHead>Produto</TableHead>
+              <TableHead>Preço</TableHead>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 14 }).map((_, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>83u8sjads</TableCell>
+                    <TableCell>Produto {i}</TableCell>
+                    <TableCell>R$ 200,00</TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default App
