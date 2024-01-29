a7468007b493a79d8eb40b78c4ff11b2522ea77d
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-29 14:31:42 +0000

    :boom: fix: edit base

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  "base": "/teste-shadcn-ui-reactjs-frontend",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "base": "/teste-shadcn-ui-reactjs-frontend",
     },
   },
 })
